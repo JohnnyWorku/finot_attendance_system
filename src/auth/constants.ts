8409fd3a042a5873b7e81048241ff86b7b255672
JohnnyWorku <Johnnythegold41@gmail.com>
2026-03-26 18:05:16 +0300

    auth constant created

---
+++ src/auth/constants.ts
@@ -0,0 +1,4 @@
+export const jwtConstants = {
+  // eslint-disable-next-line prettier/prettier
+  secret: "DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.",
+};
