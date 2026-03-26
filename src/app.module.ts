c688d93ea880b6fcb391c9a6c30fd5f277a8e7c1
JohnnyWorku <Johnnythegold41@gmail.com>
2026-03-26 16:39:41 +0300

    app.module updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule } from "@nestjs/config";
+import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
 
 @Module({
   imports: [
@@ -18,6 +20,10 @@ import { ConfigModule } from "@nestjs/config";
       entities: ["dist/**/*.entity{.ts,.js}"],
       synchronize: true,
     }),
+
+    AuthModule,
+
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
