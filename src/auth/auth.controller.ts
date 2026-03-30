cf5a8d46bab0ca8921aa4268d1723137b3bd9b1d
JohnnyWorku <Johnnythegold41@gmail.com>
2026-03-30 10:06:16 +0300

    just mere updates

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,26 @@
 import { Controller, Post, HttpCode, HttpStatus } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { signInDto } from "./dto/sign-in.dto";
+import { Public } from "src/decorators/public.decorator";
 
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  // signup
+  @Public()
+  @Post("signup")
+  signUp() {}
+
+  // signin
   @HttpCode(HttpStatus.OK)
+  @Public()
   @Post("login")
   signIn(signInDto: signInDto) {
     return this.authService.signIn(signInDto.userId, signInDto.password);
   }
+
+  // signout
+  @Post("logout")
+  signOut() {}
 }
