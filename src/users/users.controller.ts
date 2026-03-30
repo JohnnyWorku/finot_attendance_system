385c09fdd4d2048bcbfa38273b271e1c37ee224c
JohnnyWorku <Johnnythegold41@gmail.com>
2026-03-30 12:36:29 +0300

    just mere update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,7 +12,7 @@ import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { usersRole } from "src/enums/users.roles.enum";
-import { Public } from "src/decorators/public.decorator";
+// import { Public } from "src/decorators/public.decorator";
 import { Roles } from "src/decorators/roles.decorator";
 
 @Controller("users")
@@ -25,8 +25,8 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
-  @Public()
   @Get()
+  @Roles(usersRole.ADMIN)
   findAll(@Query("role") role?: usersRole) {
     return this.usersService.findAll(role);
   }
