75b10f8ff7628d0b8121de4f0c79c1258a193eb8
JohnnyWorku <Johnnythegold41@gmail.com>
2026-03-30 10:07:39 +0300

    updating the user dto and entity

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,5 @@
+import { Type } from "class-transformer";
 import {
-  IsDateString,
   IsEmail,
   IsEnum,
   IsNotEmpty,
@@ -7,11 +7,20 @@ import {
   IsString,
   IsStrongPassword,
   MaxDate,
+  Matches,
+  IsDate,
 } from "class-validator";
 import { otherSundaySchoolEnrollment } from "src/enums/other.sunday.school.enrollment.enum";
 import { usersRole } from "src/enums/users.roles.enum";
 
 export class CreateUserDto {
+  @IsString()
+  @Matches(/^ft_[a-z]{3}_[a-z]{2}_[0-9]{3}_(admin|student)$/, {
+    message:
+      "userId must be like ft_yoh_wo_001_admin (format: ft_xxx_xx_000_role)",
+  })
+  userId!: string;
+
   @IsString()
   @IsNotEmpty()
   userFullName!: string;
@@ -45,12 +54,10 @@ export class CreateUserDto {
   address!: string;
 
   @IsOptional()
-  @IsDateString(
-    {},
-    { message: "dateOfBirth must be a valid date string (YYYY-MM-DD)" },
-  )
+  @Type(() => Date)
+  @IsDate({ message: "dateOfBirth must be a valid date string (YYYY-MM-DD)" })
   @MaxDate(new Date(), { message: "dateOfBirth cannot be in the future" })
-  dateOfBirth?: string;
+  dateOfBirth?: Date;
 
   @IsEnum(otherSundaySchoolEnrollment, {
     message: "valid answer required (yes or no)",
