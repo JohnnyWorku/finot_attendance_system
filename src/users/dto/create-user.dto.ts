81f54d99d2d66365abf45d9e2e95cf09375edb75
JohnnyWorku <Johnnythegold41@gmail.com>
2026-03-30 12:39:05 +0300

    mere update

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -7,7 +7,6 @@ import {
   IsString,
   IsStrongPassword,
   MaxDate,
-  Matches,
   IsDate,
 } from "class-validator";
 import { otherSundaySchoolEnrollment } from "src/enums/other.sunday.school.enrollment.enum";
@@ -15,11 +14,8 @@ import { usersRole } from "src/enums/users.roles.enum";
 
 export class CreateUserDto {
   @IsString()
-  @Matches(/^ft_[a-z]{3}_[a-z]{2}_[0-9]{3}_(admin|student)$/, {
-    message:
-      "userId must be like ft_yoh_wo_001_admin (format: ft_xxx_xx_000_role)",
-  })
-  userId!: string;
+  @IsNotEmpty()
+  userFullName!: string;
 
   @IsOptional()
   @IsString()
