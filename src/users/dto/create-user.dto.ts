2684b9b49f7ab9c724feead66637752eff904c93
JohnnyWorku <Johnnythegold41@gmail.com>
2026-03-30 07:45:37 +0300

    updating the dtos

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,68 @@
-export class CreateUserDto {}
+import {
+  IsDateString,
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsStrongPassword,
+  MaxDate,
+} from "class-validator";
+import { otherSundaySchoolEnrollment } from "src/enums/other.sunday.school.enrollment.enum";
+import { usersRole } from "src/enums/users.roles.enum";
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  userFullName!: string;
+
+  @IsOptional()
+  @IsString()
+  userPhone?: string;
+
+  @IsOptional()
+  @IsEmail()
+  userEmail?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  fatherName!: string;
+
+  @IsOptional()
+  @IsString()
+  fatherPhone?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  motherName!: string;
+
+  @IsOptional()
+  @IsString()
+  motherPhone?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  address!: string;
+
+  @IsOptional()
+  @IsDateString(
+    {},
+    { message: "dateOfBirth must be a valid date string (YYYY-MM-DD)" },
+  )
+  @MaxDate(new Date(), { message: "dateOfBirth cannot be in the future" })
+  dateOfBirth?: string;
+
+  @IsEnum(otherSundaySchoolEnrollment, {
+    message: "valid answer required (yes or no)",
+  })
+  otherSundaySchoolEnrollment!: otherSundaySchoolEnrollment;
+
+  @IsEnum(usersRole, {
+    message: "valid role required (student or admin)",
+  })
+  userRole!: usersRole;
+
+  @IsStrongPassword()
+  @IsNotEmpty()
+  password!: string;
+}
