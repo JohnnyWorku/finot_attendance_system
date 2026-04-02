44ef56e29e009c5d3d66de8b4e398bb9bf816b5a
JohnnyWorku <Johnnythegold41@gmail.com>
2026-04-02 16:36:13 +0300

    attendance entity created

--- src/attendances/entities/attendance.entity.ts
+++ src/attendances/entities/attendance.entity.ts
@@ -1,1 +1,43 @@
-export class Attendance {}
+import { attendanceStatus } from "src/enums/attendance.status.enum";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { AttendanceSession } from "./attendance.session.entity";
+import { User } from "src/users/entities/user.entity";
+
+@Entity()
+export class Attendance {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @Column({ nullable: true })
+  checkInTime!: Date;
+
+  @Column({
+    type: "enum",
+    enum: attendanceStatus,
+    default: attendanceStatus.ABSENT,
+  })
+  checkInStatus!: attendanceStatus;
+
+  @Column({ nullable: true })
+  checkOutTime!: Date;
+
+  @Column({
+    type: "enum",
+    enum: attendanceStatus,
+    default: attendanceStatus.ABSENT,
+  })
+  checkOutStatus!: attendanceStatus;
+
+  @Column({ nullable: true })
+  remark!: string;
+
+  @ManyToOne(() => AttendanceSession, (session) => session.records, {
+    onDelete: "CASCADE",
+  })
+  session!: AttendanceSession;
+
+  @ManyToOne(() => User, (user) => user.attendances, {
+    eager: true,
+  })
+  user!: User;
+}
