03b87a7d893f8c46ea548e03eb5238f5ca033016
JohnnyWorku <Johnnythegold41@gmail.com>
2026-04-02 15:55:06 +0300

    user id generated imported and exported

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,12 @@ import { UsersService } from "./users.service";
 import { UsersController } from "./users.controller";
 import { User } from "./entities/user.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserIdGenerator } from "./helper_functions/user.id.generator";
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
-  exports: [UsersService],
+  providers: [UsersService, UserIdGenerator],
+  exports: [UsersService, UserIdGenerator],
 })
 export class UsersModule {}
