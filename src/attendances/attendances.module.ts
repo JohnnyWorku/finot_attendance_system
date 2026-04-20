120f78ad0b3fa8935fe2f9499fdf0fe8661e4b80
JohnnyWorku <Johnnythegold41@gmail.com>
2026-04-20 16:08:12 +0300

    attendance module updated for import

--- src/attendances/attendances.module.ts
+++ src/attendances/attendances.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AttendancesService } from './attendances.service';
-import { AttendancesController } from './attendances.controller';
+import { Module } from "@nestjs/common";
+import { AttendanceService } from "./attendances.service";
+import { AttendanceController } from "./attendances.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Attendance } from "./entities/attendance.entity";
+import { AttendanceSession } from "./entities/attendance-session.entity";
+import { User } from "src/users/entities/user.entity";
+import { UsersModule } from "src/users/users.module";
+import { SessionController } from "./sessions.controller";
+import { SessionService } from "./sessions.service";
 
 @Module({
-  controllers: [AttendancesController],
-  providers: [AttendancesService],
+  imports: [
+    TypeOrmModule.forFeature([Attendance, AttendanceSession, User]),
+    UsersModule,
+  ],
+  controllers: [AttendanceController, SessionController],
+  providers: [AttendanceService, SessionService],
 })
 export class AttendancesModule {}
