ead2b884f089a102e7893250795a17d0987fafc6
JohnnyWorku <Johnnythegold41@gmail.com>
2026-04-20 20:34:35 +0300

    entity name updated

--- src/attendances/attendances.module.ts
+++ src/attendances/attendances.module.ts
@@ -3,17 +3,14 @@ import { AttendanceService } from "./attendances.service";
 import { AttendanceController } from "./attendances.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Attendance } from "./entities/attendance.entity";
-import { AttendanceSession } from "./entities/attendance-session.entity";
+import { Session } from "./entities/session.entity";
 import { User } from "src/users/entities/user.entity";
 import { UsersModule } from "src/users/users.module";
 import { SessionController } from "./sessions.controller";
 import { SessionService } from "./sessions.service";
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Attendance, AttendanceSession, User]),
-    UsersModule,
-  ],
+  imports: [TypeOrmModule.forFeature([Attendance, Session, User]), UsersModule],
   controllers: [AttendanceController, SessionController],
   providers: [AttendanceService, SessionService],
 })
