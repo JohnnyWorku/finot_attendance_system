b4ac54823b9675b7fdfa349117ea6fb09f5cc020
JohnnyWorku <Johnnythegold41@gmail.com>
2026-04-20 16:18:50 +0300

    user module updated

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,7 +3,7 @@ import { UsersService } from "./users.service";
 import { UsersController } from "./users.controller";
 import { User } from "./entities/user.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { UserIdGenerator } from "./helper_functions/user.id.generator";
+import { UserIdGenerator } from "./helper_functions/user-id-generator";
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
